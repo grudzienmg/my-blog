0ecbaeb8c70bfba96d434aea5a639c7349745bb2
Marta Grudzien <maarta.grudzien@gmail.com>
2020-03-26 13:49:29 +0100

    Post component added

---
+++ src/components/posts/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Posts = ({...props}) => {
+  return (<div>test123</div>);
+}
+
+export default Posts;
---
+++ src/styles/mainPage.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const MainPage = styled.div`
+  width: 90vh;
+  height: 90vh;
+  margin: auto;
+  margin-top: 5vh;
+  padding: 20px;
+  overflow: auto;
+  box-shadow: 5px 5px 5px lightgray;
+`;
+
+export default MainPage;
